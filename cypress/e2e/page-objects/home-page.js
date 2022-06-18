ad658c94e164d1942495132b299a97f55d8230af
Michael Robson <michael.r@sliide.com>
2022-06-18 23:03:59 +0100

    Initial setup

--- cypress/e2e/page-objects/home-page.js
+++ cypress/e2e/page-objects/home-page.js
@@ -2,11 +2,13 @@ import selectors from "../test-data/home-page-data.js";
 
 class HomePage {
   navigateToHome() {
+    // In Cypress when you visit / it means visit the base url. This is set it cypress.config.js or can be overwritten with an env variable.
     cy.visit("/");
   }
 
   clickContactUsLink() {
     // Reads the contactUsLink selector from the imported file then finds and clicks the first instance of it
+    // Note usually we wouldn't need to get the first instance but the Sliide website is a bit strnage everything is in the code twice.
     cy.get(selectors.contactUsLink).first().click();
   }
 
